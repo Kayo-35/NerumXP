a1183aa5f49e049dcd90faa48f00f7c80539fa03
Kayo Almeida Medvedchikoff <kayoalmeida35@gmail.com>
2025-07-17 20:25:05 -0300

    Revert "feat: Reverter push indevido: Migrações básicas"
    This reverts commit 2219c824c84f4e1e55bcee4be7f07a286918ea68.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,22 +7,12 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="container mt-5">
-        <h1>Assinaturas</h1>
-        <table class="table">
-            <thead class="table-primary">
-                <th>Código</th>
-                <th>Nome</th>
-            </thead>
-            <tbody>
-                @foreach($assinaturas as $assinatura)
-                    <tr>
-                        <td>{{ $assinatura['cd_assinatura'] }}</td>
-                        <td>{{ $assinatura['nm_assinatura'] }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+    <table class="table">
+        <tr>
+            <td>Ola</td>
+            <td>Como</td>
+            <td>Vai?</td>
+        </tr>
+    </table>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Conta\Assinatura;
 
 Route::get("/", function () {
-    $assinaturas = Assinatura::all();
-    return view("home",["assinaturas" => $assinaturas]);
+    return view("home");
 });
